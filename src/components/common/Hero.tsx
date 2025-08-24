dec0ea2333eb11903dd4ef02689189acabb33d98
MattSnyder97 <mattsnyderspam@gmail.com>
2025-08-24 11:27:26 -0400

    Hero fix

--- src/components/common/Hero.tsx
+++ src/components/common/Hero.tsx
@@ -23,7 +23,7 @@ export default function Hero() {
      
       {/* Content */}
       <div className="container-padding mx-auto relative z-10 flex items-center min-h-[520px] md:min-h-[525px]">
-        <div className="text-center md:text-left text-offWhite max-w-[900px] mx-auto md:mx-0">
+        <div className="text-center md:text-left text-offWhite max-w-[550px] mx-auto md:mx-0">
             <h1 className="text-5xl md:text-6xl font-bold tracking-tight mb-4">
                 Giving Hope
             </h1>
@@ -31,7 +31,7 @@ export default function Hero() {
                 Through Community and Research
             </h2>
           <p className="text-base md:text-lg leading-relaxed text-offWhite/80 max-w-prose mb-8 mx-auto md:mx-0">
-            Our foundation exists to speed up finding the cure for <span className="font-semibold">small fiber neuropathy</span> and provide you the support you need along the way.
+            Our foundation exists to speed up finding the cure for small fiber neuropathy and provide you the support you need along the way.
           </p>
           <div className="flex flex-col items-center gap-8 md:flex-row md:items-start md:flex-wrap md:gap-8 md:justify-start">
             <Button
